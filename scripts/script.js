fb11186242fa24402efd8a0061bfbcc4d1d5576f
ben-rieth <benrieth3@gmail.com>
2022-04-04 23:04:54 -0500

    Create seperate function for building divs

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,32 @@ function colorDiv(evt) {
     evt.currentTarget.classList.add('colored');
 }
 
+function createDiv() {
+    let div = document.createElement('div');
+    div.classList.add('block');
+    div.addEventListener('mouseover', colorDiv);
+    return div;
+}
+
+function createBreak() {
+    let breakDiv = document.createElement('div');
+    breakDiv.classList.add('break');
+    return breakDiv;
+}
+
 function createGrid(size=16) {
     const container = document.getElementById('container');
     let totalSquares = Math.pow(size, 2);
 
-    let div;
-    let breakDiv;
     for(let i = 1; i <= totalSquares; i++) {
-        div = document.createElement('div');
-        div.classList.add('block');
-        div.addEventListener('mouseover', colorDiv); 
-        container.appendChild(div);
+        container.appendChild(
+            createDiv()
+        );
 
         if (i % size == 0) {
-            breakDiv = document.createElement('div');
-            breakDiv.classList.add('break');
-            container.appendChild(breakDiv);
+            container.appendChild(
+                createBreak()
+            );
         }
     }
 }
