e356643b12bf02ce35a0d9cd454d5c507f3b0dd3
ben-rieth <benrieth3@gmail.com>
2022-04-04 23:01:42 -0500

    Color in squares when moused over

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+function colorDiv(evt) {
+    evt.currentTarget.classList.add('colored');
+}
+
 function createGrid(size=16) {
     const container = document.getElementById('container');
     let totalSquares = Math.pow(size, 2);
@@ -7,6 +11,7 @@ function createGrid(size=16) {
     for(let i = 1; i <= totalSquares; i++) {
         div = document.createElement('div');
         div.classList.add('block');
+        div.addEventListener('mouseover', colorDiv); 
         container.appendChild(div);
 
         if (i % size == 0) {
